0d30f37484d1c8625adea99ff7349c1861d86d6e
florian <lemiere.florian@gmail.com>
2020-10-13 17:30:49 +0200

    add texte

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a href="http://google.fr" target={"_blank"}>Mingindindayo</a>
+          <p>Yolo !!!!!!</p>
       </header>
     </div>
   );
